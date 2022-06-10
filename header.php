e0bf70862b2060bbe0052a03b7f93f51b70c937b
Swikarr <emailaddr>
2022-06-10 12:11:59 +0545

    initail commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <center>
+    <a href="index.php">Addition</a> | <a href="sub.php"> SUBSTRACTION</a> | <a href="mul.php">MULTIPLICATION</a> | <a href="div.php">DIVISION</a> 
+    <title>Math Flashcard</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include "header.php"; 
+       require "random.php";
+       ;?>
+
+
+<h1> ADDITION </h1>
+<h1>
+    <?php 
+    echo $num1 . "+". $num2;
+    ?>
+</h1>
+<br> <br><br>
+
+<?php 
+if(isset($_POST['answer'])):
+    
+
+$correct_answer = $_POST["num1"]+$_POST["num2"];
+
+
+if ($correct_answer == $_POST["answer"]){
+    echo "Correct!  ".$_POST["num1"] . " + " . $_POST["num2"] . " = " . $correct_answer; }
+    else {
+    echo "Wrong  ".$_POST["num1"] . " + " . $_POST["num2"] . " =  " . $correct_answer." NOT  ".$_POST["answer"];
+}
+
+?>
+<?php endif; ?>
+
+<form method="post" action ="">
+<input name="num1" type="hidden" value="<?php echo $num1; ?>">
+<input name="num2" type="hidden" value="<?php echo $num2; ?>">
+    Answer: <input name="answer">
+    <button> Submit</button>
+    <button> New Card </button> 
+</form>
+
+<?php include "footer.php"; ?>
+</center>
+
+
+</body>
+</html>
